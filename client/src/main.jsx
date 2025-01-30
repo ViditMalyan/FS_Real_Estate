e9ea5ca65a1ad79f880ab0bb2a9cb2e1c7679b16
Vidit Malyan <viditmalyan@gmail.com>
2025-01-30 20:33:08 +0530

    Initial commit

---
+++ client/src/main.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import {Auth0Provider} from "@auth0/auth0-react";
+// import { MantineProvider } from "@mantine/core";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    {/* <MantineProvider> */}
+    <Auth0Provider
+    domain="dev-dk8d3zuqwf8rwspg.us.auth0.com"
+    clientId="N0I5ZZ81nsaq8ZeolfPsFIRqhRAo0Nlw"
+    authorizationParams={{
+      redirect_uri: "http://localhost:5173"
+    }}
+    audience="http://localhost:8000/"
+    scope="openid profile email">
+    <App />
+    </Auth0Provider>
+    {/* </MantineProvider> */}
+  </React.StrictMode>
+);
